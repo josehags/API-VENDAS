96ab0219d9f2a4ce9a01fefc704393b2bf03dadc
josehags <104442763+josehags@users.noreply.github.com>
2022-06-29 11:04:51 -0300

    alteração mail.ts

--- src/config/mail/mail.ts
+++ src/config/mail/mail.ts
@@ -13,8 +13,8 @@ export default {
 
     defaults: {
         from: {
-            email: 'contato@aluiziodeveloper.cf',
-            name: 'Jorge Aluizio',
+            email: 'henriquealvesguedes@hotmail.com',
+            name: 'José Henrique',
         },
     },
 } as IMailConfig;
